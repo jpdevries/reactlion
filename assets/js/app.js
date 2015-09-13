47f8aa39fdd63d7c143e5d2ea8b9bac8ae949c95
JP DeVries <johnpaul.devries@gmail.com>
2015-09-12 23:45:29 -0700

    Deleting work they'll need to do themselves

--- assets/js/app.js
+++ assets/js/app.js
@@ -522,6 +522,49 @@ var lastMousePos = new Point(window.innerWidth * .5, window.innerHeight * .5); /
 var mouseVelocity = new Point(Math.random() * (window.innerWidth * .4), Math.random() * (window.innerHeight * .4)); // base the initial mouse velocity off screensize
 var breakability = .9; // decimal between 0-1, closer to 1 the further polygons push away from eachother
 
+function getTransform(movable,polarX,polarY) {
+    if(!movable) return ('translate(0,0)');
+    
+    var x = mouseVelocity.x;
+    var y = mouseVelocity.y;
+    
+    if(polarX) x = 0-x;
+    if(polarY) y = 0-y;
+    
+    x *= movability.x;
+    y *= movability.y;
+    
+    return ('translate(' + x + ' ' + y + ')');
+}
+
+function LightenDarkenColor(col, amt) { //https://css-tricks.com/snippets/javascript/lighten-darken-color/
+    var usePound = false;
+
+    if (col[0] == "#") {
+        col = col.slice(1);
+        usePound = true;
+    }
+
+    var num = parseInt(col,16);
+
+    var r = (num >> 16) + amt;
+
+    if (r > 255) r = 255;
+    else if  (r < 0) r = 0;
+
+    var b = ((num >> 8) & 0x00FF) + amt;
+
+    if (b > 255) b = 255;
+    else if  (b < 0) b = 0;
+
+    var g = (num & 0x0000FF) + amt;
+
+    if (g > 255) g = 255;
+    else if (g < 0) g = 0;
+
+    return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
+}
+
 setInterval(function(){ // wait 4 seconds then start listening to mouse move
     document.body.onmousemove = function(e) {    
         var mousePos = new Point(e.screenX, e.screenY); // current mouse position
@@ -532,99 +575,12 @@ setInterval(function(){ // wait 4 seconds then start listening to mouse move
     }
 },4000);
 
-var Polygon = React.createClass({displayName: "Polygon",
-    getInitialState:function(){
-        return {
-            origFill:this.props.fill, // store the original fill color
-            origPoints:this.props.points, // store the original positions that the polygon should be attracted to
-            darkAmnt:0, // a number we will continue to increment and that then take the Math.sin() value of
-            colorVelocity:randomRange(0.04,0.13), // how quickly to change the colors
-            colorReach:randomRange(8,24), // how much the hue should be aloud to shift
-            transform:'translate(0 0)', // transform amount, initially nothing. used to move the peices
-            movable:(Math.random() <= .75), // whether or not the polygon is movable
-            movability:new Point(randomRange(0.86,1),randomRange(0.86,1)), // how "movable" each polygon is
-            polarity:new Point(Math.random() <= .5, Math.random() <= .5) // mouse polarity (which way to go when the mouse is moved)
-        }
-    },
-    render:function() {
-          // some otherwise unnecessary pointer variables to make things more legible
-          var destColor = this.state.origFill;
-          var colorVelocity = this.state.colorVelocity;
-          var colorReach = this.state.colorReach;
-          var movable = this.state.movable;
-          var polarity = this.state.polarity;
-          var movability = this.state.movability;
-          
-          // increment the darken amount. this number keeps getting bigger forever, but we take the sin of it so it fluctuates gradually between -1 and 1
-          this.state.darkAmnt += colorVelocity;
-    
-          // calculate the destination color
-          destColor = LightenDarkenColor(destColor,(Math.sin(this.state.darkAmnt) * colorReach));
-      
-          // let React do its thing
-          return React.createElement("polygon", {fill: destColor, transform: getTransform(movable,polarity.x,polarity.y), points: this.state.origPoints});
-    
-          function getTransform(movable,polarX,polarY) {
-              if(!movable) return ('translate(0,0)');
-              
-              var x = mouseVelocity.x;
-              var y = mouseVelocity.y;
-              
-              if(polarX) x = 0-x;
-              if(polarY) y = 0-y;
-              
-              x *= movability.x;
-              y *= movability.y;
-              
-              return ('translate(' + x + ' ' + y + ')');
-          }
-          
-          function LightenDarkenColor(col, amt) { //https://css-tricks.com/snippets/javascript/lighten-darken-color/
-              var usePound = false;
-
-              if (col[0] == "#") {
-                  col = col.slice(1);
-                  usePound = true;
-              }
-
-              var num = parseInt(col,16);
-
-              var r = (num >> 16) + amt;
-
-              if (r > 255) r = 255;
-              else if  (r < 0) r = 0;
-
-              var b = ((num >> 8) & 0x00FF) + amt;
-
-              if (b > 255) b = 255;
-              else if  (b < 0) b = 0;
-
-              var g = (num & 0x0000FF) + amt;
-
-              if (g > 255) g = 255;
-              else if (g < 0) g = 0;
-
-              return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
-          }
-    }
-});
-
-var SuperCoolLion = React.createClass({displayName: "SuperCoolLion",
-    render:function() {
-        return (React.createElement("div", null, React.createElement(SVGArt, null)));
-    }
-});
-
-var superCoolLion = document.getElementById('super-cool-lion');
-
 function step(timestamp) { // onEnterFrame
-    React.render(React.createElement(SuperCoolLion, null), superCoolLion); // trigger a repaint
-    
-    window.requestAnimationFrame(step); // keep the clock ticking
-    
     // apply friction to mouse
     mouseVelocity.x *= 0.86;
     mouseVelocity.y *= 0.86;
+    
+    window.requestAnimationFrame(step); // keep the clock ticking
 }
 
 // get the party started
